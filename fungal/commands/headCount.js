eb26bf846599a1e89e0ba5f91ff8f1892642eaf0
Yukitoh <maximeq@hotmail.fr>
2020-02-22 21:57:43 +0100

    Update headCount.js

--- fungal/commands/headCount.js
+++ fungal/commands/headCount.js
@@ -9,6 +9,7 @@ async function main(spt, data){
 	.then(async function (mentionHere) {
 		await mentionHere.delete();
 	})
+	afkCheckChannel.send(`this is a hc?`);
 	afkCheckChannel.send({ embed })
 	.then(async function (headcount) {
 		await reactHC(spt, headcount);
