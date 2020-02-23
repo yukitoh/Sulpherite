51d9f9d7178f3fb97d748569c10a17cc58d0a1d6
Yukitoh <maximeq@hotmail.fr>
2020-02-23 14:15:19 +0100

    fix

--- fungal/commands/location.js
+++ fungal/commands/location.js
@@ -3,7 +3,7 @@ const afkChecks = require("../commands.js").afkChecks;
 
 async function location(spt, data, args){
 	if (data.content.substr((args[0].length+1)).length > 3){
-		var location = data.content.substr((args[0].length+1);
+		var location = data.content.substr((args[0].length+1));
 		console.log(`new location is:`+location);
 	}
 }
