52f5ed559bde61452bd0a3af931e7226e425ed70
Yukitoh <maximeq@hotmail.fr>
2020-02-23 14:26:22 +0100

    finalizing location

--- fungal/commands/location.js
+++ fungal/commands/location.js
@@ -1,10 +1,34 @@
 const config = require("../../config.json");
 const afkChecks = require("../commands.js").afkChecks;
+const forceUpdate = require("../commands.js").updateAfkObjs;
 
 async function location(spt, data, args){
 	if (data.content.substr((args[0].length+1)).length > 3){
 		var location = data.content.substr((args[0].length+1));
-		console.log(`new location is:`+location);
+		var authorafk = data.author.id;
+		var currAfkCheckObj;
+		for (x in afkChecks) {
+			if (afkChecks[x]['host'] == authorafk){
+				var currAfkCheckObj = afkChecks[x]
+			}
+		}
+		if (currAfkCheckObj == undefined){
+			data.channel.send(`You are not the host of any current afk check.`);
+		} else {
+			// set location
+			currAfkCheckObj['location'] = location;
+			// pm key / nitro / rusher
+			if (currAfkCheckObj['key'] != 'None') currAfkCheckObj['key'].send(`Location has been updated, it is now: \`${location}\``);
+			if (currAfkCheckObj['rusher'] != 'None') currAfkCheckObj['rusher'].send(`Location has been updated, it is now: \`${location}\``);
+			if (currAfkCheckObj['nitro'].length > 0){
+				for (x in currAfkCheckObj['nitro']){
+					currAfkCheckObj['nitro'][x].send(`Location has been updated, it is now: \`${location}\``);
+				}
+			}
+
+			data.channel.send(`Location has been successfuly updated.`);
+			forceUpdate(spt, false);
+		}
 	}
 }
 
