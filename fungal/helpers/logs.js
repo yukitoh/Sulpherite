e9e73db28b4de54861296af9488c6f4f26624cd6
Yukitoh <maximeq@hotmail.fr>
2020-02-27 23:53:39 +0100

    Update logs.js

--- fungal/helpers/logs.js
+++ fungal/helpers/logs.js
@@ -2,10 +2,10 @@ const db = require('../../db.js');
 
 async function logs(spt, data){
 	if (err) throw err;
-	db.query(`SELECT * FROM keys`, function (err, result, fields){
-		if (err) throw err;
+	db.query(`SELECT * FROM keyers`, function (result, fields){
 		console.dir(result);
 	})
+	data.channel.send(`Logs I've recorded so far: ${result.length} keys`);
 }
 
 module.exports = logs;
