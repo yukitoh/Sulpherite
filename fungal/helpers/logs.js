5ab2915a72147c183685494d82648c6a8ecc85fd
Yukitoh <maximeq@hotmail.fr>
2020-02-28 00:39:30 +0100

    Update logs.js

--- fungal/helpers/logs.js
+++ fungal/helpers/logs.js
@@ -10,25 +10,25 @@ async function logs(spt, data){
 			var successes = 0;
 			var keys = 0;
 
-			if (resultkeys != undefined){
+			if (resultrls != undefined && resultrls.length > 0){
 				fails = resultrls.forEach(rl => {
 					if (rl.fail > 0) fails += rl.fail;
 				})
 			}
 
-			if (resultrls != undefined){
+			if (resultrls != undefined && resultrls.length > 0){
 				successes = resultrls.forEach(rl => {
 					if (rl.success > 0) successes += rl.success;
 				})
 			}
 
-			if (resultkeys != undefined){
+			if (resultkeys != undefined && resultkeys.length > 0){
 				keys = resultkeys.forEach(key => {
 					if (key.amount > 0) keys += key.amount;
 				})
 			}
-			
-			await data.channel.send(`**Logs I've recorded so far:**\n*${keys}* keys, *${successes}* successes and *${fails}* fails.`);
+
+			await data.channel.send(`**Logs I've recorded so far:**\n${keys} keys, ${successes} successes and ${fails} fails.`);
 		})
 	})
 }
