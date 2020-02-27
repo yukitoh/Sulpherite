e17741dbabdc77877a5462387f66885c7fd30206
Yukitoh <maximeq@hotmail.fr>
2020-02-27 23:58:57 +0100

    Update logs.js

--- fungal/helpers/logs.js
+++ fungal/helpers/logs.js
@@ -1,7 +1,7 @@
 const db = require('../../db.js');
 
 async function logs(spt, data){
-	con.connect(function(err) {
+	db.connect(function(err) {
 		if (err) throw err;
 		db.query(`SELECT * FROM keyers`, function (err, result, fields){
 			if (err) throw err;
