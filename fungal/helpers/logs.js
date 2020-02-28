1f3ba167e6fb73aee0616790a583064c66d4f9b9
Yukitoh <maximeq@hotmail.fr>
2020-02-28 01:09:40 +0100

    Update logs.js

--- fungal/helpers/logs.js
+++ fungal/helpers/logs.js
@@ -1,28 +1,22 @@
 const db = require('../../db.js');
 
 async function logs(spt, data){
-	await db.query('SELECT * FROM keyers', async function (err, resultkeys, fields){
-		console.dir(resultkeys);
-		await db.query('SELECT * FROM rls', async function (err, resultrls, fields){
-			console.dir(resultrls);
-
-			var fails = 0;
-			var successes = 0;
-			var keys = 0;
-
-			if (resultrls){
-				resultrls.forEach(rl => {
+	await db.query(`SELECT * FROM keyers`, async function (err, reskeys, fields){
+		await db.query(`SELECT * FROM rls`, async function (err, resrls, fields){
+			var fails = 0,
+				successes = 0,
+				keys = 0;
+			if (resrls){
+				resrls.forEach(rl => {
 					fails += rl.fail;
 					successes += rl.success;
 				})
 			}
-
-			if (resultkeys){
-				resultkeys.forEach(key => {
+			if (reskeys){
+				reskeys.forEach(key => {
 					keys += key.amount;
 				})
 			}
-
 			await data.channel.send(`**Logs I've recorded so far:**\n${keys} keys, ${successes} successes and ${fails} fails.`);
 		})
 	})
