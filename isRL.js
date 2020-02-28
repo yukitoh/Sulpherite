55fa3dd07fe053ec96bc2ccc8d7a922c01e751fc
Yukitoh <maximeq@hotmail.fr>
2020-02-28 02:47:26 +0100

    Update isRL.js

--- isRL.js
+++ isRL.js
@@ -1,28 +1,33 @@
 const config = require("./config.json");
 
 async function main(spt, server, user){
-	if (server == 'fungal'){
-		var guildmb = spt.guilds.get(config.fungal.id).members.get(user);
-		if (guildmb.roles.some(r => [config.fungal.arlRole, config.fungal.rlRole, config.fungal.vrlRole, config.fungal.hrlRole].includes(r.name))){
-			return true;
-		} else {
-			return false;
+	console.log(server);
+	switch(server){
+		case 'fungal':
+			var guildmb = spt.guilds.get(config.fungal.id).members.get(user);
+			if (guildmb.roles.some(r => [config.fungal.arlRole, config.fungal.rlRole, config.fungal.vrlRole, config.fungal.hrlRole].includes(r.name))){
+				return true;
+			} else {
+				return false;
+			}
+			break;
+		case 'shatters':
+			var guildmb = spt.guilds.get(config.shatters.id).members.get(user);
+			if (guildmb.roles.some(r => [config.shatters.arlRole, config.shatters.rlRole, config.shatters.vrlRole, config.shatters.hrlRole].includes(r.name))){
+				return true;
+			} else {
+				return false;
+			}
+			break;
+		case 'hispano':
+			var guildmb = spt.guilds.get(config.hispano.id).members.get(user);
+			if (guildmb.roles.some(r => [config.shatters.rlRole].includes(r.name))){
+				return true;
+			} else {
+				return false;
+			}
+			break;
 		}
-	} else if (server == 'shatters'){
-		var guildmb = spt.guilds.get(config.shatters.id).members.get(user);
-		if (guildmb.roles.some(r => [config.shatters.arlRole, config.shatters.rlRole, config.shatters.vrlRole, config.shatters.hrlRole].includes(r.name))){
-			return true;
-		} else {
-			return false;
-		}
-	} else if (server == 'hispano'){
-		var guildmb = spt.guilds.get(config.hispano.id).members.get(user);
-		if (guildmb.roles.some(r => [config.shatters.rlRole].includes(r.name))){
-			return true;
-		} else {
-			return false;
-		}
-	}
 }
 
 module.exports = main;
