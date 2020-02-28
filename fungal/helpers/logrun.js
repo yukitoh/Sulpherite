f48c2cddf4555da6c1e59dbda494a355b5b3e325
Yukitoh <maximeq@hotmail.fr>
2020-02-28 01:57:33 +0100

    fix

--- fungal/helpers/logrun.js
+++ fungal/helpers/logrun.js
@@ -1,10 +1,16 @@
+const config = require('../../config.json');
 const db = require('../../db.js');
 
-async function log(spt, data){
-	if ()
-	await db.query(`SELECT * FROM rls`, async function (err, resrls, fields){
-		
-	})
+async function logrun(spt, data){
+	var args = data.split(' ');
+
+	if (!args[1]){
+		return data.channel.send('Please specify a type of run (available: fc).');
+	}
+	if (!args[2] || args[2] != 'f' && args[2] != 's' || args[2] != 'fail' && args[2] != 's'){
+		return data.channel.send(`Please specify if run was a success or a fail.`);
+	}
+	if (args[3])
 }
 
-module.exports = log;
+module.exports = logrun;
