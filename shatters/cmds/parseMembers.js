f53cb7789a3675edb7654d016c105bb0e24dbecf
Yukitoh <maximeq@hotmail.fr>
2020-03-15 22:59:38 +0100

    Update parseMembers.js

--- shatters/cmds/parseMembers.js
+++ shatters/cmds/parseMembers.js
@@ -35,9 +35,9 @@ async function main(spt, data, args){
 
 			if (ingamescreen){
 				data.channel.send(`Starting to find members now...`);
-				var apiUrl = `https://api.ocr.space/parse/imageurl?apikey=4c09473bec88957&url=${ingamescreen}&scale=true`;
+				var apiUrl = `https://api.ocr.space/parse/imageurl?apikey=4c09473bec88957&url=${ingamescreen}&filetype=PNG&isOverlayRequired=true&scale=true`;
 				console.log(apiUrl);
-				
+
 				https.get(apiUrl, (resp) => {
 					let data = '';
 
