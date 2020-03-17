f080b178b2989fe16968db4ff6c83b9085cae539
Yukitoh <maximeq@hotmail.fr>
2020-03-18 00:23:53 +0100

    xd

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ spt.on('ready', () => {
 	}, 300000); // every 5 minutes (300000
 
 	console.log('Sulpherite is running!')
-	spt.user.setActivity(config.status.replace('$user', spt.users.size), { type: ``})
+	spt.user.setActivity(config.status, { type: ``})
 	setInterval(function() {
 		// Handling afk checks update
         require("./shatters/cmds.js").updAfkObj(spt, true);
